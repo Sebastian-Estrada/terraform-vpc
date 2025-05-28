3873b76e40fa59a31ef298771279f4281d2a403e
Sebastian Estrada <sestrada@danalyticspro.co>
2025-05-27 21:33:48 -0400

    style: aplica formato con terraform fmt para cumplir validación del CI

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -1,19 +1,19 @@
 provider "aws" {
-    region = var.aws_region
-    profile = "terraform-training"
+  region  = var.aws_region
+  profile = "terraform-training"
 }
 
 module "vpc" {
-    source = "../../modules/vpc"
-    name     = "vpc-dev"
-    vpc_cidr_block = var.vpc_cidr_block
+  source         = "../../modules/vpc"
+  name           = "vpc-dev"
+  vpc_cidr_block = var.vpc_cidr_block
 }
 
 module "public_subnet" {
   source            = "../../modules/subnet"
   name              = "public-subnet-dev"
   vpc_id            = module.vpc.vpc_id
-  cidr_block       = var.cidr_block
+  cidr_block        = var.cidr_block
   availability_zone = var.availability_zone
   public            = true
 }
