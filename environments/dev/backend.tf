7df28ee38aa33a02eeef2f6ff12ef9dd5bf326a6
Sebastian Estrada <sestrada@danalyticspro.co>
2025-05-28 15:32:46 -0400

    fix: Se cambia el nombre del bucket del backend

--- environments/dev/backend.tf
+++ environments/dev/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "terraform-remote-state-sebastian"
+    bucket         = "terraform-remote-state-sebastian-2"
     key            = "vpc/dev/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "terraform-locks"
